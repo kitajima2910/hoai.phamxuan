c8d5db7132c72709c43f58bdf3adf3b24832ed14
kitajima2910 <kitajima2910@gmail.com>
2023-06-07 23:32:17 +0700

    - Create a library in C++

---
+++ C/Libraries/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "PXH2910.h"
+
+using namespace std;
+using namespace PXH;
+
+int main() {
+
+  test();
+
+  return 0;
+}
---
+++ C/Libraries/PXH2910.h
@@ -0,0 +1,10 @@
+#ifndef PXH2910_H_
+#define PXH2910_H_
+
+namespace PXH {
+  void test() {
+    std::cout << "Hello World!";
+  }
+}
+
+#endif
