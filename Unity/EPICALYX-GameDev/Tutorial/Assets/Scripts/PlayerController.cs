f45ca205739f203e3f4a0c3ad93900627954d6b0
kitajima2910 <kitajima2910@gmail.com>
2023-07-07 20:08:42 +0700

    - Character animation

--- Unity/EPICALYX-GameDev/Tutorial/Assets/Scripts/PlayerController.cs
+++ Unity/EPICALYX-GameDev/Tutorial/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private bool isMoving;
     private Vector2 input;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -16,10 +22,16 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            //Debug.Log("This is input.x: " + input.x);
+            //Debug.Log("This is input.y: " + input.y);
+
             if (input.x != 0) input.y = 0;
 
             if (input != Vector2.zero)
             {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
@@ -28,6 +40,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        animator.SetBool("isMoving", isMoving);
     }
 
     IEnumerator Move(Vector3 targetPos)
@@ -36,7 +49,7 @@ public class PlayerController : MonoBehaviour
 
         while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.fixedDeltaTime);
             yield return null;
         }
         transform.position = targetPos;
