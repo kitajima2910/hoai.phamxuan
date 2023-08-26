79a5e7922d8d3c5674ce7cc872aa8e74a75fbaa9
kitajima2910 <kitajima2910@gmail.com>
2023-08-26 22:46:56 +0700

    - CANVAS Creating Data Graphes & Diagrams

---
+++ HTML/Canvas/BananaCoding/js/main5.js
@@ -0,0 +1,42 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let window_height = window.innerHeight;
+let window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.backgroundColor = "#ddf";
+
+const data = [
+    200, 150, 170, 100, 80, 50, 350, 200, 200, 230
+];
+
+
+const start_value = data[0];
+const distance = canvas.width / data.length;
+const start_point = 0;
+
+context.beginPath();
+
+context.lineWidth = 3;
+context.strokeStyle = "#f56";
+
+context.moveTo(start_point, start_value);
+
+data.forEach((element, index) => {
+    const new_distance = start_point + (distance * (index + 1));
+    context.lineTo(new_distance, element);
+});
+
+
+context.lineTo(canvas.width, canvas.height);
+context.lineTo(start_point, canvas.height);
+context.lineTo(start_point, start_value);
+
+context.fillStyle = "grey";
+context.fill();
+
+context.stroke();
+context.closePath();
