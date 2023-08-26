9a597dad83978822774145d47a33d692919daed5
kitajima2910 <kitajima2910@gmail.com>
2023-08-26 16:57:13 +0700

    - Create classes and object circles dynamic

---
+++ HTML/Canvas/BananaCoding/js/main2.js
@@ -0,0 +1,48 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+let window_height = window.innerHeight;
+let window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.backgroundColor = "#ff8";
+
+
+class Circle {
+
+    constructor(xpos, ypos, radius, color) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(context) {
+        context.beginPath();
+        context.lineWidth = 5;
+        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+        context.stroke();
+        context.closePath();
+    }
+
+}
+
+
+let all_circles = [];
+
+let createCircle = function(circle) {
+    circle.draw(context);
+}
+
+for(let number = 0; number < 10; number++) {
+    let random_x = Math.random() * window_width;
+    let random_y = Math.random() * window_height;
+
+    let my_circle = new Circle(random_x, random_y, 50, "black");
+    all_circles.push(my_circle);
+    createCircle(all_circles[number]);
+}
+
+console.log(all_circles);
