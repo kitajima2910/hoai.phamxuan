73767a090fc9804ab26d43effb242fbdb83b2df3
kitajima2910 <kitajima2910@gmail.com>
2024-01-06 17:51:13 +0700

    - Kiểu Dữ Liệu

---
+++ Python/Khoa_hoc_python_co_ban_de_viet_tool/KieuDuLieu.py
@@ -0,0 +1,59 @@
+# Số nguyên: integer
+# a = 10 + 30
+# print(a)
+
+# Số thực: float
+# a = 2.3 - 2.4
+# print(a)
+
+# Chuỗi: string
+# a = 'tool'
+
+# Nối chuỗi
+# a += ' python'
+
+# Phân thân =]]z
+# print((a + ' ') * 2)
+
+# Kiểm tra độ dài chuỗi
+# print(len(a))
+
+# print(a)
+
+
+# Kiểu dữ liệu dạng danh sách: list
+# a = [2, 3, 1, 5, 2, 5, 1]
+
+# Độ dài list
+# print(len(a))
+
+# Lấy giá trị trong list
+# Vị trí 0 -> cuối (thuận)
+# Vị trí -1 -> 0 (đếm xuôi, -1 là vị trí cuối) 
+# print(a[-1])
+
+# Kiểu tuple giống list
+# a = (2, 3)
+# print(a[1])
+
+# a, b, c = (2, 3, 1)
+# print(c, b, a)
+
+# Kiểu boolean: True/False
+# a = True
+# b = False
+# print(a, b)
+
+# Kiểu dictionary: key - value
+# a = {
+#     'a': 'chữ a',
+#     'b': 'chữ b',
+#     5: 'số năm'
+# }
+
+# print(a['a'])
+# print(a['b'])
+# print(a[5])
+
+# Cách kiểm tra kiểu dữ liệu dùng type
+print(type(a))
