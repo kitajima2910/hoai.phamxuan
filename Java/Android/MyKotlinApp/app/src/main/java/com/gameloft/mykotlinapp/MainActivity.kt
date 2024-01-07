87a7c9af5ffadbb0ead300391e045920d1c27c0c
kitajima2910 <kitajima2910@gmail.com>
2024-01-07 18:52:01 +0700

    - 2024-01-07

---
+++ Java/Android/MyKotlinApp/app/src/main/java/com/gameloft/mykotlinapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.gameloft.mykotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var  btnAdd: Button
+    lateinit var  btnSub: Button
+    lateinit var  btnMultiply: Button
+    lateinit var  btnDivision: Button
+
+    lateinit var etA: EditText
+    lateinit var etB: EditText
+
+    lateinit var resultTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtraction)
+        btnMultiply = findViewById(R.id.btn_multiplication)
+        btnDivision = findViewById(R.id.btn_division)
+
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+
+        resultTV = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMultiply.setOnClickListener(this)
+        btnDivision.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+
+        var a = etA.text.toString()
+        var b = etB.text.toString()
+
+        if(a.isEmpty() || b.isEmpty()) {
+
+        } else {
+            var num_a = a.toDouble()
+            var num_b = b.toDouble()
+
+            var result = 0.0
+
+            when(v?.id){
+                R.id.btn_add -> { result = num_a + num_b }
+                R.id.btn_subtraction -> { result = num_a - num_b }
+                R.id.btn_multiplication -> { result = num_a * num_b }
+                R.id.btn_division -> { result = num_a / num_b }
+            }
+
+            resultTV.text = "Result is ${result}"
+        }
+
+    }
+}
